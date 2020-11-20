eb94ec07c69b3db0f2b2864761a51b5cbb11114d
snjssk <snjssk0403@gmail.com>
2020-11-20 23:48:18 +0900

    update

--- main.js
+++ main.js
@@ -4,10 +4,11 @@ const ejs = require('ejs')
 const app = express()
 
 // ejs setting
-app.set('views engine', 'ejs')
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs')
 
 // static files
-app.use(express.static('public'))
+app.use(express.static(path.join(__dirname, 'public')));
 
 // json
 app.get('/', (req, res) => {
@@ -18,12 +19,13 @@ app.get('/', (req, res) => {
 
 // html file
 app.get('/html', (req, res) => {
-  res.sendFile(path.resolve(__dirname, 'src/pages/index.html'))
+  // res.sendFile(path.resolve(__dirname, 'src/pages/index.html'))
+  res.render('./index')
 })
 app.get('/about', (req, res) => {
   res.sendFile(path.resolve(__dirname, 'src/pages/about.html'))
 })
 
-app.listen(13000, () => {
+app.listen(18080, () => {
   console.log('App listening')
 })
